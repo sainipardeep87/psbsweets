2755a7574d94aaf182b3435a3fad1a9d5cfa6775
Pardeep Saini <saini.pardeep87@gmail.com>
2020-09-14 11:24:50 +0530

    design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   images = [
-    {'image': 'assets/images/indenpendance.jpg', 'label': 'test label 2', 'desc': 'test description 2'},
-    {'image': 'assets/images/cake.jpg', 'label': 'Delicious Cakes ', 'desc': 'Fresh and delicious cakes are ready 24 hours'},
-    {'image': 'assets/images/display1.jpg', 'label': 'test label 2', 'desc': 'test description 2'},
+    {'image': 'assets/images/indenpendance.jpg', 'label': ' l', 'desc': ''},
+    {'image': 'assets/images/cake.jpg', 'label': '', 'desc': ''},
+    {'image': 'assets/images/display1.jpg', 'label': '', 'desc': ''},
     // {'image': 'assets/images/temp-images/slide-1-flex.jpg', 'label': 'test label 1', 'desc': 'test descripition'},
     // {'image': 'assets/images/temp-images/slide-2-flex.jpg', 'label': 'test label 2', 'desc': 'test description 2'}
   ];
