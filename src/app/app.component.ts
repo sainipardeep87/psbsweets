f249752574e257c5ed9df3ada656f69ce07eed24
Pardeep Saini <saini.pardeep87@gmail.com>
2020-09-14 11:12:18 +0530

    slider images changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   images = [
-    {'image': 'assets/images/temp-images/slide-1-flex.jpg', 'label': 'test label 1', 'desc': 'test descripition'},
-    {'image': 'assets/images/temp-images/slide-2-flex.jpg', 'label': 'test label 2', 'desc': 'test description 2'}
+    {'image': 'assets/images/indenpendance.jpg', 'label': 'test label 2', 'desc': 'test description 2'},
+    {'image': 'assets/images/cake.jpg', 'label': 'Delicious Cakes ', 'desc': 'Fresh and delicious cakes are ready 24 hours'},
+    {'image': 'assets/images/display1.jpg', 'label': 'test label 2', 'desc': 'test description 2'},
+    // {'image': 'assets/images/temp-images/slide-1-flex.jpg', 'label': 'test label 1', 'desc': 'test descripition'},
+    // {'image': 'assets/images/temp-images/slide-2-flex.jpg', 'label': 'test label 2', 'desc': 'test description 2'}
   ];
 }
